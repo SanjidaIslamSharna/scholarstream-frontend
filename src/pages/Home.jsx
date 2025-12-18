2d1cca533dd202ad84c423195dd638f4d7741d0a
SanjidaIslamSharna <sanjidaislamsharna20@gmail.com>
2025-12-18 13:54:01 +0600

    update file path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Banner from '../components/Home/Banner';
-import TopScholarships from '../components/Home/TopScholarships';
-import SuccessStories from '../components/Home/SuccessStories';
+import Banner from '../components/home/Banner';
+import TopScholarships from '../components/home/TopScholarships';
+import SuccessStories from '../components/home/SuccessStories';
 import FAQ from '../components/Home/FAQ';
 import { motion } from 'framer-motion';
 
